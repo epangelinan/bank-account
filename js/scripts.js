fa8f57907cda269c252b49b2ad4389fe0ef67e3f
Esvethlynna Pangelinan <esveth@aol.com>
2017-06-14 01:16:23 -0700

    finished logic for deposit and withdrawal

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ function BankAccount (name, balance) {
   this.balance = balance;
 }
 
+BankAccount.prototype.currentBalance = function(amount){
+  $("#balance").text(amount);
+}
 
 BankAccount.prototype.deposit = function (depositAmount) {
   this.balance += depositAmount;
@@ -14,13 +17,48 @@ BankAccount.prototype.withdraw = function (withdrawalAmount) {
   return this.balance;
 }
 
-// get name and initial deposit
-var name = $("#inputName").val();
-var initialDeposit = $("#inputInitialDeposit").val();
 
-var newBankAccount = new BankAccount(name, initialDeposit);
-console.log(newBankAccount);
-//
+
+// get name and initial deposit
+
+
+
+
+$(document).ready(function() {
+  //alert("ready function")
+  $("form#register").submit(function() {
+    event.preventDefault();
+    var inputtedName = $("#inputName").val();
+    var inputtedInitialDeposit = parseInt($("#inputInitialDeposit").val());
+    //alert("register submit");
+    //alert(inputtedName);
+    //alert(inputtedInitialDeposit);
+    newBankAccount = new BankAccount(inputtedName, inputtedInitialDeposit);
+    console.log(newBankAccount);
+    newBankAccount.currentBalance(inputtedInitialDeposit);
+    return newBankAccount;
+  });
+  $("form#depositOrWithdraw").submit(function() {
+    event.preventDefault();
+    //alert("Deposit/Withdraw submit");
+    var inputtedDeposit = parseInt($("#inputDeposit").val());
+    var inputtedWithdrawal = parseInt($("#inputWithdrawal").val());
+
+    if ((inputtedDeposit > 0) && (inputtedWithdrawal === NaN)) {
+      newBankAccount.currentBalance(newBankAccount.deposit(inputtedDeposit));
+    }
+
+    if ((inputtedDeposit === NaN) && (inputtedWithdrawal > 0)) {
+      newBankAccount.currentBalance(newBankAccount.withdraw(inputtedWithdrawal));
+    }
+
+    if ((inputtedDeposit > 0) && (inputtedWithdrawal > 0)) {
+      newBankAccount.currentBalance(newBankAccount.deposit(inputtedDeposit));
+      newBankAccount.currentBalance(newBankAccount.withdraw(inputtedWithdrawal));
+    }
+
+  });
+});
 // newBankAccount.deposit(200);
 // console.log(newBankAccount.balance);
 //
